2754b0f65fd3658e25335e84b88bad3b87908442
yoshietao <yoshietao@g.ucla.edu>
2018-03-07 17:53:06 -0800

    restart

---
+++ func.py
@@ -0,0 +1,72 @@
+import datetime, time
+import pytz
+import json
+from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+pst_tz = pytz.timezone('US/Pacific')
+
+'''
+class Data:
+	def __init__(self):
+		with open('tweets_#nfl.txt') as json_data:
+			self.nfl = [json.loads(line) for line in json_data]
+		with open('tweets_#superbowl.txt') as json_data:
+			self.superbowl = [json.loads(line) for line in json_data]
+'''
+def load_q1_1(filename):
+	if Path('./data/'+filename+'Q1_1.npy').exists():
+		print('Already have Q1_1 data.')
+		return np.load('./data/'+filename+'Q1_1.npy')
+	else:
+		print('Parsing Q1_1 data...')
+		d = []
+		with open(filename+'.txt') as data:
+			for line in data:
+				line = json.loads(line)
+				d.append([datetime.datetime.fromtimestamp(line['citation_date'], pst_tz).strftime('%Y-%m-%d %H:%M:%S')[5:13], line['author']['followers'], line['metrics']['citations']['total']])
+		np.save('./data/'+filename+'Q1_1.npy',np.array(d))
+		return np.load('./data/'+filename+'Q1_1.npy')
+
+def load_q1_2(filename):
+	if Path('./data/'+filename+'Q1_2x.npy').exists():
+		print('Already have Q1_2 data.')
+		return np.load('./data/'+filename+'Q1_2x.npy'),np.load('./data/'+filename+'Q1_2y.npy')
+	else:
+		print('Parsing Q1_2 data...')
+		x = OrderedDict()
+		y = OrderedDict()
+		with open(filename+'.txt') as data:
+			for line in data:
+				line = json.loads(line)
+				index = datetime.datetime.fromtimestamp(line['citation_date'], pst_tz).strftime('%Y-%m-%d %H:%M:%S')[5:13]
+				if index not in x:
+					x[index] = [0,0,0,0,int(index[6:8])]
+				x[index][0] += 1
+				x[index][1] += int(line['metrics']['citations']['total'])
+				x[index][2] += float(line['author']['followers'])
+				if float(line['author']['followers']) > x[index][3]:
+					x[index][3] = float(line['author']['followers'])
+		x = np.array(list(x.values())).astype('int')
+		y = x[:,0]
+		np.save('./data/'+filename+'Q1_2x.npy',x[:-1,:])
+		np.save('./data/'+filename+'Q1_2y.npy',y[1:])
+		print(x,y,x.shape,y.shape)
+		return np.load('./data/'+filename+'Q1_2x.npy'), np.load('./data/'+filename+'Q1_2y.npy')
+
+def plot_histogram(d):
+	fig,ax = plt.subplots()
+	plt.bar(np.arange(len(d)),list(d.values()))
+	ax.set_xlabel('hours')
+	ax.set_ylabel('tweets')
+	ax.set_title('Number of tweets in hour')
+	plt.show()
+
+def main():
+	d_nfl = load_d('tweets_#nfl')
+	d_superbowl = load_d('tweets_#superbowl')
+
+if __name__ == '__main__':
+	main()
+	
---
+++ main.py
@@ -0,0 +1,44 @@
+import func
+from collections import OrderedDict
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.utils import shuffle
+
+
+
+
+def Q1(d):
+	d1 = OrderedDict()
+	for tweet in d:
+		d1[tweet[0]] = d1[tweet[0]]+1 if tweet[0] in d1 else 1
+	print('Average number of tweets per hour:', sum(d1.values())/len(d1))
+	print('Average number of followers of users posting the tweets:', sum(d[:,1].astype('float'))/d.shape[0])
+	print('Average number of retweets:',sum(d[:,2].astype('int'))/d.shape[0])
+	func.plot_histogram(d1)
+
+def Q2():
+	d_nfl = func.load_q1_2('tweets_#nfl')
+	d_superbowl = func.load_q1_2('tweets_#superbowl')
+	q2(d_nfl)
+	q2(d_superbowl)
+
+def q2(d):		#d[0] = x, d[1] = y
+	lrm = LinearRegression(fit_intercept=True, normalize=True)
+	x,y = shuffle(x,y,random_state=42)
+
+
+
+
+
+
+
+def main():
+	#d_nfl = func.load_q1_1('tweets_#nfl')
+	#d_superbowl = func.load_q1_1('tweets_#superbowl')
+	#Q1(d_nfl)
+	#Q1(d_superbowl)
+	Q2()
+
+
+if __name__ == '__main__':
+	main()
